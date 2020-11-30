0cf080d141afe1a69f201504cb7636f9371a1e25
AnnaButkevich <Butkevi4.anna@gmail.com>
2020-12-01 00:36:57 +0100

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import Car from "./components/car/car";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    cars = [
+        {title : 'Mersedes', model : '25lk85-52', year : '2015', power : 1.6, color : 'black'},
+        {title : 'BMW', model : '25lk85-52', year : '2015', power : 2.6, color : 'black'},
+        {title : 'Volkswagen', model : 'Toureg', year : '2012', power : 2.5, color : 'red'},
+        {title : 'Shkoda', model : 'Fabia', year : '2015', power : 1.6, color : 'white'},
+        {title : 'Volkswagen', model : 'Golf 4', year : '2001', power : 1.6, color : 'silver'},
+    ]
+    render() {
+        return (
+            <div>
+                {
+                    this.cars.map((value, index) => {
+                        let name = 'target'
+                        if (index % 2) {
+                            name = 'other'
+                        }
+                        return (<Car item = {value} key ={index} clsName = {name}/>)
+                    })
+                }
+            </div>
+        );
+    }
 }
 
 export default App;
